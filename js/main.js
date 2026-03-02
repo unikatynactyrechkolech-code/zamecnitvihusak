3de9cdfab864ef6c288578cbc256e69119d3b524
Unikaty na ctyrech kolech <unikatynactyrechkolech@gmail.com>
2026-03-02 12:41:03 +0100

    UI fixes: contact form borders, faster animations, better service layout spacing

--- js/main.js
+++ js/main.js
@@ -70,8 +70,8 @@ function initScrollAnimations() {
     const animatedElements = document.querySelectorAll('.animate-fade-in, .animate-slide-up, .animate-slide-left, .animate-slide-right, .animate-scale-up');
     
     const observerOptions = {
-        threshold: 0.1,
-        rootMargin: '0px 0px -50px 0px'
+        threshold: 0.01,
+        rootMargin: '0px 0px 100px 0px'
     };
     
     const observer = new IntersectionObserver((entries) => {
